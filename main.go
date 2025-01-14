10748f196df5370ec5999fa6b7b19d2079648db8
Atticus Pomerantz <atticuspomerantz@gmail.com>
2025-01-14 11:31:30 -0600

    additional functions

--- main.go
+++ main.go
@@ -5,51 +5,25 @@ import (
 	"strings"
 )
 
+const confrenceTickets = 50
+
+var confrenceName = "Go Confrence" // shorthand declaration invalid for package level variables
+var remainingTickets = 50
+var bookings []string //this is a slice, similar to array but with dynamic size
+
 func main() {
-	//	var confrenceName = "Go Confrence"
-	confrenceName := "Go Confrence" // shorthand declaration
-	const confrenceTickets = 50
-	var remainingTickets = 50
-	var bookings []string //this is a slice, similar to array but with dynamic size
 
-	greetUsers(confrenceName, confrenceTickets, remainingTickets)
+	greetUsers()
 
-	for {
-		var firstName string
-		var lastName string
-		var userTickets int
-		var email string
-
-		fmt.Println("Enter your first name")
-		fmt.Scan(&firstName) //user input pointer
-
-		fmt.Println("Enter your last name")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Enter your email")
-		fmt.Scan(&email)
-
-		fmt.Println("Enter number of tickets")
-		fmt.Scan(&userTickets)
-
-		isValidName := len(lastName) >= 2 && len(firstName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTickets := userTickets > 0 && userTickets <= remainingTickets
+	for { //infinite loop
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTickets := validateUserInput(firstName, lastName, email, userTickets)
 
 		if isValidEmail && isValidName && isValidTickets {
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName) //append to the slice. have to assign back to variable
 
-			fmt.Printf("Thank you %s %s for booking %d tickets for %s\n", firstName, lastName, userTickets, confrenceName)
-			fmt.Printf("We have %d tickets remaining out of a total of %d\n", remainingTickets, confrenceTickets)
+			bookTicket(userTickets, firstName, lastName, email)
 
-			firstNames := []string{}
-
-			for _, booking := range bookings {
-				//_ is a throwaway variable (blank identifier)
-				var names = strings.Fields(booking) //creates an array of strings from the booking string
-				firstNames = append(firstNames, names[0])
-			}
+			firstNames := getFirstNames()
 
 			fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
@@ -64,8 +38,57 @@ func main() {
 	}
 }
 
-func greetUsers(confrenceName string, remainingTickets int, confrenceTickets int) {
+func greetUsers() {
 	fmt.Println("!!!Welcome to our", confrenceName, "booking application")
 	fmt.Println("We have", remainingTickets, "tickets remaining out of a total of", confrenceTickets)
 	fmt.Println("Get your tickets now")
+}
+
+func getFirstNames() []string {
+	firstNames := []string{}
+
+	for _, booking := range bookings {
+		//_ is a throwaway variable (blank identifier)
+		var names = strings.Fields(booking) //creates an array of strings from the booking string
+		firstNames = append(firstNames, names[0])
+	}
+
+	return firstNames
+}
+
+func getUserInput() (string, string, string, int) {
+	var firstName string
+	var lastName string
+	var userTickets int
+	var email string
+
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName) //user input pointer
+
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func validateUserInput(firstName string, lastName string, email string, userTickets int) (bool, bool, bool) {
+	isValidName := len(lastName) >= 2 && len(firstName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTickets := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTickets //go can return multiple values
+}
+
+func bookTicket(userTickets int, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName) //append to the slice. have to assign back to variable
+
+	fmt.Printf("Thank you %s %s for booking %d tickets for %s, we will email you at %s\n", firstName, lastName, userTickets, confrenceName, email)
+	fmt.Printf("We have %d tickets remaining out of a total of %d\n", remainingTickets, confrenceTickets)
 }
